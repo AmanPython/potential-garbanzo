b71c5dcf414994acb45840164c1184edf7a328fc
Aman Gora <amangora1999@gmail.com>
2023-09-22 12:06:08 +0530

    subsetSums naming conventions

--- Recursion/Day9/subsetSums.py
+++ Recursion/Day9/subsetSums.py
@@ -1,14 +1,15 @@
 class Solution:
-	def subsetSums(self, arr, N):
-		# code here
-		all_sol = []
-		def helper_fun(totalsum,n):
-			if n >= N:
-				all_sol.append(totalsum)
-				return 
-			helper_fun(totalsum + arr[n],n+1)
-			helper_fun(totalsum,n+1)
-		helper_fun(0,0)
-		return all_sol
-	
-	
+    def subsetSums(self, arr, N):
+        all_sums = []  # List to store all subset sums
+        
+        def generate_subset_sums(current_sum, index):
+            if index >= N:
+                all_sums.append(current_sum)  # Add the current sum to the list of subset sums
+                return 
+            # Include the current element in the subset sum
+            generate_subset_sums(current_sum + arr[index], index + 1)
+            # Exclude the current element from the subset sum
+            generate_subset_sums(current_sum, index + 1)
+        
+        generate_subset_sums(0, 0)  # Start the recursion with initial sum 0 and index 0
+        return all_sums
