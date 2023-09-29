b82eb98d78fb69be15bb345d8cf6b8e506ca1273
Aman Gora <amangora1999@gmail.com>
2023-09-29 12:52:30 +0530

    subset sum

--- AdityaVerma/subset_sum.py
+++ AdityaVerma/subset_sum.py
@@ -3,17 +3,20 @@ import unittest
 class Solution:
     def isSubsetSum (self, N, arr, sum):
         # code here 
-        # Recursive Solution
-        if sum == 0:
-            return True
-        elif N == 0 and sum != 0:
-            return False
-        if sum - arr[N-1] >= 0:
-            return self.isSubsetSum(N-1,arr,sum-arr[N-1]) or self.isSubsetSum(N-1,arr,sum)
-        else:
-            return self.isSubsetSum(N-1,arr,sum)
+        dp = [[0]*(sum+1) for _ in range(N+1)]
         
-
+        ## initialize dp
+        for i in range(N+1):
+            dp[i][0] = 1
+            
+        for i in range(1,N+1):
+            for j in range(1,sum+1):
+                if j - arr[i-1] >= 0:
+                    dp[i][j] = max(dp[i-1][j-arr[i-1]],dp[i-1][j])
+                else:
+                    dp[i][j] = dp[i-1][j]
+        return dp[N][sum]
+        
 class Test(unittest.TestCase,Solution):
     def test_isSubsetSum(self):
         self.assertEqual(self.isSubsetSum(6,[3, 34, 4, 12, 5, 2],9),1)
